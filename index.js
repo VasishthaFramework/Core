32bab8b5f6f889e626ad3498a8917f993a06fa45
archanpatkar <pat@archanpatkar.com>
2018-08-16 09:21:34 +0530

    Update

---
+++ index.js
@@ -0,0 +1,6 @@
+const V = require('./Framework/V')
+const Controller = require('./Framework/Controller');
+const Request = require('./Framework/Request');
+const Response = require('./Framework/Response');
+
+module.exports = { V, Controller, Request, Response }
