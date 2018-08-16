cba82a8230ceec1b4f71ed9353773c925e32ee3c
archanpatkar <pat@archanpatkar.com>
2018-08-16 16:48:55 +0530

    Old Code

---
+++ Backyard/V.js
@@ -0,0 +1,19 @@
+            // if(this.controllers[path] != undefined)
+            // {
+            //     if(this.controller[path] instanceof Array)
+            //     {
+            //         this.controllers[path].push(new controller());
+            //     }
+            //     else 
+            //     {
+            //         const temp = this.controllers[path];
+            //         this.controllers[path] = [];
+            //         if ( Array.isArray(temp) ) this.controllers[path].push(...temp);
+            //         else this.controllers[path].push(temp);
+            //         this.controllers[path].push(new controller());
+            //     }
+            // }
+            // else
+            // {
+            //     this.controllers[path] = controller; 
+            // }
