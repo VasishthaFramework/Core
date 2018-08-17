6807f03b0c5a7a89d3f8ea9a2d8d790a2b0fb7c3
archanpatkar <pat@archanpatkar.com>
2018-08-17 16:55:33 +0530

    Testing Global Store

--- Test/controllers/a1.controller.js
+++ Test/controllers/a1.controller.js
@@ -5,6 +5,12 @@ class A1 extends Controller
     get(req,res)
     {
         res.append("<h1> Entering A1 </h1>");
+        const message = req.global.message;
+        const times = req.global.times;
+        for(let i = 0; i < times;i++)
+        {
+            res.append("<h2>" + message + "</h2>");    
+        }
         res.append("<h1> Leaving A1 </h1>");
     }
 }
