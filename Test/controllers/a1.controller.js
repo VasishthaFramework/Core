49bdf2ce73dbb668198b28f5ce58259a6c00bd6c
archanpatkar <pat@archanpatkar.com>
2018-08-17 08:14:11 +0530

    Inter Controller Communication Test

--- Test/controllers/a1.controller.js
+++ Test/controllers/a1.controller.js
@@ -4,17 +4,8 @@ class A1 extends Controller
 {
     get(req,res)
     {
-        res.setHeader('Content-Type','text/html');
-        let msg = req.query.msg;
-        let times = req.query.times;
-        if(msg === undefined) msg = "Bye World!";
-        if(times === undefined) times = 10;
-        else times = parseInt(times);
-        for(let i = 0;i < times;i++)
-        {
-            res.append("<h1>" + msg + "</h1>");
-        }
-        res.send();
+        res.append("<h1> Entering A1 </h1>");
+        res.append("<h1> Leaving A1 </h1>");
     }
 }
 
