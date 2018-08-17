5144db77fc88b8a145e486e816bd1770ed1eed7b
archanpatkar <pat@archanpatkar.com>
2018-08-17 08:14:17 +0530

    Inter Controller Communication Test

--- Test/controllers/c1.controller.js
+++ Test/controllers/c1.controller.js
@@ -5,30 +5,9 @@ class C1 extends Controller
     get(req,res)
     {
         res.setHeader('Content-Type','text/html');
-        let msg = req.query.msg;
-        let times = req.query.times;
-        if(msg === undefined) msg = "Hello World!";
-        if(times === undefined) times = 10;
-        else times = parseInt(times);
-        for(let i = 0;i < times;i++)
-        {
-            res.append("<h1>" + msg + "</h1>");
-        }
-        res.send();
-    }
-
-    post(req,res)
-    {
-        res.setHeader('Content-Type','text/html');
-        let msg = req.post.msg;
-        let times = req.post.times;
-        if(msg === undefined) msg = "Hello World!";
-        if(times === undefined) times = 10;
-        else times = parseInt(times);
-        for(let i = 0;i < times;i++)
-        {
-            res.append("<h1>" + msg + "</h1>");
-        }
+        res.append("<h1> Entering C1 </h1>");
+        req.RequestDispatcher("/Bye").forward(req,res);
+        res.append("<h1> Leaving C1 </h1>");
         res.send();
     }
 }
