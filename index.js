97d5ad9871310772b6d7a2fbd7319bf9d9089b4a
archanpatkar <pat@archanpatkar.com>
2018-08-18 23:51:04 +0530

    Removing unessesary exports

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const V = require('./Framework/V')
 const Controller = require('./Framework/Controller');
-const Request = require('./Framework/Request');
-const Response = require('./Framework/Response');
 
-module.exports = { V, Controller, Request, Response }
+module.exports = { V, Controller }
