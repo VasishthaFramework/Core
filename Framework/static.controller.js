0917257e2a443ae4da9a0ad44d88e7c0f9aa2f77
archanpatkar <pat@archanpatkar.com>
2018-08-18 22:25:17 +0530

    Issue #9 , #15 and #16 Solved

---
+++ Framework/static.controller.js
@@ -0,0 +1,58 @@
+const Controller = require('./Controller');
+const fs = require('fs');
+const path = require("path");
+const mime = require('mime-types');
+
+class StaticController extends Controller
+{
+    init(config)
+    {
+        const folder = config.folder;
+        const abspath = path.resolve(folder);
+        const files  = fs.readdirSync(folder);
+        for(let file of files)
+        {
+            this.addFile({ path:file , filepath:`${abspath}/${file}` });
+        }
+        fs.watch(abspath, (event, file) => {
+            if (event === 'rename') {
+              const filepath = `${abspath}/${file}`;
+              if (fs.existsSync(filepath)) {
+                console.log(`Serving a new file: ${file}`);
+                this.addFile({ path:file , filepath:filepath });
+              } else {
+                console.log(`Stopped Serving: ${file}`);
+                this.removeFile(`/${file}`);
+              }
+            }
+          }
+        );
+    }
+
+    addFile({ path , filepath })
+    {
+        if(this.files === undefined) this.files = {};
+        path = "/" + path;
+        this.files[path] = filepath; 
+    }
+
+    removeFile(path)
+    {
+        delete this.files[path];
+    }
+
+    get(req,res)
+    {
+        if(this.files !== undefined)
+        {
+            const filepath = this.files[req.path];
+            const data = fs.readFileSync(filepath).toString();
+            const type = mime.contentType(path.extname(filepath));
+            res.setHeader('Content-Type',type);
+            res.append(data);
+            res.close();
+        }
+    }
+}
+
+module.exports = { path:"static" , controller: StaticController };
