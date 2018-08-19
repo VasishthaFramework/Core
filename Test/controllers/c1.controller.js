5dbdfb1f3912d2e97f7e75e68f635439c074cb90
archanpatkar <pat@archanpatkar.com>
2018-08-19 14:57:30 +0530

    Testing

--- Test/controllers/c1.controller.js
+++ Test/controllers/c1.controller.js
@@ -6,10 +6,9 @@ class C1 extends Controller
     {
         res.setHeader('Content-Type','text/html');
         res.append("<h1> Entering C1 </h1>");
-        req.global.times = 5;
-        req.RequestDispatcher("/Bye").forward(req,res);
+        req.view = "one";
+        req.RequestDispatcher("view").forward(req,res);
         res.append("<h1> Leaving C1 </h1>");
-        res.close();
     }
 }
 
