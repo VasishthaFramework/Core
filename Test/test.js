5999ee5f728340a914a8db215d3d39fb7d458f8d
archanpatkar <pat@archanpatkar.com>
2018-08-19 14:57:27 +0530

    Testing

--- Test/test.js
+++ Test/test.js
@@ -1,5 +1,5 @@
 const { V } = require("../index");
 const app = new V();
-app.load("./controllers").static("./public");
+app.load("./controllers").static("./public").views("./views");
 app.global.message = "Hello World!";
 app.start(9081).then( (server) => console.log("Server has started on PORT: 9081") );
