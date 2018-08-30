fe0824677e2cdcac4827770b8c5c95bc52066c9e
archanpatkar <pat@archanpatkar.com>
2018-08-30 20:32:52 +0530

    Initial

---
+++ Framework/InfraControllers/session.controller.js
@@ -0,0 +1,17 @@
+const Controller = require("../Controller");
+
+class SessionController extends Controller
+{
+    init(config)
+    {
+        
+    }
+
+    get(req,res)
+    {  
+
+    }
+
+}
+
+module.exports = SessionController;
