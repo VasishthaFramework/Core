ece25a3c233d2f2b16c806ed600139b6b549c007
archanpatkar <pat@archanpatkar.com>
2018-09-06 22:45:08 +0530

    Update

--- Framework/InfraControllers/state.controller.js
+++ Framework/InfraControllers/state.controller.js
@@ -2,10 +2,26 @@ const Controller = require("../Controller");
 
 class StateController extends Controller
 {
-    init()
+    init(config)
     {
-        
+       // State Container Name to be provided
+       // Transformers and Mutators
+       // Rules Enforcers to initialized and Singular Level to the Container Level
     }
+
+    get(req,res)
+    {
+        const op = String(req.query.operation).toLowerCase();
+        switch(op)
+        {
+            case "create":break;
+            case "read":break;
+            case "update":break;
+            case "delete":break;
+            default:throw new Error("No Such Opertation! Only supports CRUD");
+        }
+    }
+
 }
 
-module.exports = StateController;
+module.exports = { path:"state" , controller: StateController };
