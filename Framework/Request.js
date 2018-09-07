31b478a426590a7c593b80fadd60bb22abba8c57
archanpatkar <pat@archanpatkar.com>
2018-09-07 08:47:30 +0530

    Update

--- Framework/Request.js
+++ Framework/Request.js
@@ -6,7 +6,15 @@ class Request
         this.method = req.method;
         this.url = req.url;
         this.query = route.query;
+        this.body = route.post;
         this.path = route.pathname;
+    }
+
+    param(name)
+    {
+        if(name in this.query) return this.query[name];
+        else if(name in this.body) return this.body[name];
+        return null;
     }
 }
 module.exports = Request;
